1a48e22576c7680ef8f303f32afb0e6d880443cd
Eugene Oparyshev <eop@sitecore.net>
2018-09-21 11:59:24 +0300

    initial commit

---
+++ src/Sitecore.Support.255467/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.255467")]
+[assembly: AssemblyProduct("Sitecore.Support.255467")]
+[assembly: ComVisible(false)]
